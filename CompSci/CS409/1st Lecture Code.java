52f1afffc96b68eb7d85b7e3a85f358d09856020
filip <filip.malarczyk.2022@uni.strath.ac.uk>
2025-09-23 12:18:33 +0100

    vault backup: 2025-09-23 12:18:33

--- CompSci/CS409/1st Lecture Code.java
+++ CompSci/CS409/1st Lecture Code.java
@@ -1,3 +1,17 @@
 public static void main(String args[]){
     System.out.println("Heeewo :3");
 }
+
+
+// This is the inrerface for the iterator
+public interface playListIterator{
+    boolean hasnext();
+    String next();
+}
+public interface MediaPlaylist{
+    playListIterator createIterator(String s);
+}
+
+/**
+ * Whats the benefit of using interfaces is so 
+ */
